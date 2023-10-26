cedc7e31e3f9012709fbc4b6de633b8aa28d9db8
Zuniga2044 <rz30xray@gmail.com>
2023-10-26 16:20:20 -0500

    ejercicio clase 1

---
+++ html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenidos</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <h1>Bienvenidos</h1>
+</body>
+</html>
